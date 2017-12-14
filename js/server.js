7c4c8eadca1e2b4509710c5cca94ea601369b9d1
zplett <zplett@oberlin.edu>
2017-12-14 00:08:13 -0500

    trivia

--- js/server.js
+++ js/server.js
@@ -1,20 +1,9 @@
-// Listening logic 
-const express = require( 'express' )
-const server = express()
-
-// Listen to get requests over the standard slug
-server.get( '/', ( req, rest ) => res.send( 'hello world!' ) )
-
-// Listen on 3000 
-server.listen( 3000, () => console.log( 'Listening on 3000' ) )
-
 // The client
 //var client = requestJson.createClient( 'https://boiling-cove-42309.herokuapp.com/' )
 
 //client.post( )
 
 // Requesting logic
-//request = require( 'request-json')
 const request = require('request');
 const fs = require( 'fs' )
 let stream = fs.createWriteStream( './json/trivia.json' )
@@ -22,7 +11,17 @@ request( 'https://opentdb.com/api.php?amount=50', { json: true }, (err, res, bod
          {
              
              if( err ) {  console.log( 'sucks' ); }
-             stream.write( JSON.stringify( body ) )
+             else
+             { stream.write( JSON.stringify( body ) ) }
              
          } );
 
+// Listening logic 
+const express = require( 'express' )
+const server = express()
+
+// Listen to get requests over the standard slug
+server.get( '/', ( req, res ) => res.send( 'hello world!' ) )
+
+// Listen on 3000 
+//server.listen( 3000, () => console.log( 'Listening on 3000' ) )
