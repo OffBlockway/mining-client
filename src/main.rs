43924f209eddb18cd06bbd8429248e83a5746b9c
zplett <zplett@oberlin.edu>
2017-12-14 21:24:51 -0500

    miner json correctly serializing

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn main()
 {
 
     // Registers the miner with the client 
-    register_miner();
+    // register_miner();
 
     // TODO: receive cargo
     
@@ -62,8 +62,10 @@ fn main()
 
     // TODO: have input stream use trivia database and ship package on success 
     
-    input_stream();
-    
+    // input_stream();
+    let mut miner = Miner::new();
+    miner.construct_merkle( &"./json/transactions.json" );
+    miner.write_to( &"dummy.json" ); 
 }
 
 // Registers the mining node with the full client
